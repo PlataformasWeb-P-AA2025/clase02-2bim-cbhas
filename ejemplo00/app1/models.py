b917ed82cba6bd18cdea07a073654744225a05d6
Sebastia <calderonsebastian211@gmail.com>
2025-06-11 08:48:31 -0500

    Update v.0.3

--- ejemplo00/app1/models.py
+++ ejemplo00/app1/models.py
@@ -14,3 +14,14 @@ class Estudiante(models.Model):
             self.cedula,
             self.edad
         )
+
+class Ciudad(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=30)
+    poblacion = models.CharField(max_length=30)
+
+    def __str__(self):
+        return "%s - %s" % (
+            self.nombre,
+            self.poblacion
+        )
